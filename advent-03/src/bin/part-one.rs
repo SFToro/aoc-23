8f566adee30c1143f48afe03f2adb53dff3c4bb3
Salvador Ferradás <sftpo@pm.me>
2023-12-03 20:38:03 +0100

    chore: Day 03

---
+++ advent-03/src/bin/part-one.rs
@@ -0,0 +1,118 @@
+use std::collections::{BTreeMap, BTreeSet};
+
+fn main() {
+    let input: &str = include_str!("../../input.txt");
+    println!("{}", process(input));
+}
+
+struct Last {
+    chars: Vec<char>,
+    starting_index: i32,
+}
+impl Last {
+    fn reset(&mut self) {
+        self.chars = vec![];
+        self.starting_index = -1;
+    }
+}
+
+fn process(input: &str) -> usize {
+    let lines = input.lines();
+    let mut grid: BTreeMap<(usize, usize), String> = BTreeMap::new();
+    let mut last = Last {
+        chars: vec![],
+        starting_index: -1,
+    };
+    for (i, line) in lines.enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            if c.is_digit(10) {
+                if last.starting_index == -1 {
+                    last.starting_index = j as i32;
+                }
+                last.chars.push(c);
+            } else {
+                if last.starting_index != -1 {
+                    grid.insert(
+                        (i, last.starting_index as usize),
+                        last.chars.iter().collect(),
+                    );
+                }
+                if c != '.' {
+                    grid.insert((i, j), c.into());
+                }
+                last.reset();
+            }
+        }
+        if last.starting_index != -1 {
+            grid.insert(
+                (i, last.starting_index as usize),
+                last.chars.iter().collect(),
+            );
+        }
+        last.reset();
+    }
+
+    let mut required = BTreeSet::new();
+    'outer: for (k, v) in grid.iter() {
+        let mut adjacents: Vec<(i32, i32)> = vec![];
+        if v.parse::<usize>().is_ok() {
+            let len = v.len();
+            adjacents.push((k.0 as i32 - 1, k.1 as i32 - 1));
+            adjacents.push((k.0 as i32, k.1 as i32 - 1));
+            adjacents.push((k.0 as i32 + 1, k.1 as i32 - 1));
+
+            adjacents.push((k.0 as i32 - 1, k.1 as i32 + len as i32));
+            adjacents.push((k.0 as i32, k.1 as i32 + len as i32));
+            adjacents.push((k.0 as i32 + 1, k.1 as i32 + len as i32));
+
+            for i in 0..len {
+                adjacents.push((k.0 as i32 - 1, k.1 as i32 + i as i32));
+
+                adjacents.push((k.0 as i32 + 1, k.1 as i32 + i as i32));
+            }
+
+            for adj in adjacents {
+                let (x, y) = (adj.0 as usize, adj.1 as usize);
+                if grid.contains_key(&(x, y)) {
+                    let adjascent = grid.get(&(x, y)).unwrap();
+                    if !adjascent.parse::<usize>().is_ok() {
+                        required.insert(k);
+                        continue 'outer;
+                    }
+                }
+            }
+        }
+    }
+
+    required
+        .into_iter()
+        .map(|(x, y)| {
+            if grid.contains_key(&(*x, *y)) {
+                grid.get(&(*x, *y)).unwrap().parse::<usize>().unwrap()
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT_TEXT: &str = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(process(INPUT_TEXT), 4361);
+    }
+}
---
+++ advent-03/src/bin/part-two.rs
@@ -0,0 +1,133 @@
+use std::collections::{BTreeMap, BTreeSet};
+
+fn main() {
+    let input: &str = include_str!("../../input.txt");
+    println!("{}", process(input));
+}
+
+struct Last {
+    chars: Vec<char>,
+    starting_index: i32,
+}
+impl Last {
+    fn reset(&mut self) {
+        self.chars = vec![];
+        self.starting_index = -1;
+    }
+}
+
+fn process(input: &str) -> usize {
+    let lines = input.lines();
+    let mut grid: BTreeMap<(usize, usize), String> = BTreeMap::new();
+    let mut last = Last {
+        chars: vec![],
+        starting_index: -1,
+    };
+    for (i, line) in lines.enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            if c.is_digit(10) {
+                if last.starting_index == -1 {
+                    last.starting_index = j as i32;
+                }
+                last.chars.push(c);
+            } else {
+                if last.starting_index != -1 {
+                    grid.insert(
+                        (i, last.starting_index as usize),
+                        last.chars.iter().collect(),
+                    );
+                }
+                if c != '.' {
+                    grid.insert((i, j), c.into());
+                }
+                last.reset();
+            }
+        }
+        if last.starting_index != -1 {
+            grid.insert(
+                (i, last.starting_index as usize),
+                last.chars.iter().collect(),
+            );
+        }
+        last.reset();
+    }
+
+    grid.retain(|_k, v| {
+        if v.parse::<usize>().is_ok() || *v == "*".to_string() {
+            true
+        } else {
+            false
+        }
+    });
+
+    let mut results = vec![];
+
+    let mut entries_to_insert = vec![];
+    for (k, v) in grid.iter() {
+        if v.parse::<usize>().is_ok() {
+            let len = v.len();
+            for m in 0..len {
+                entries_to_insert.push(((k.0, k.1 + m), (*v).clone()));
+            }
+        }
+    }
+    for entry in entries_to_insert {
+        grid.insert(entry.0, entry.1);
+    }
+
+    for (k, v) in grid.iter() {
+        let mut gear_adjacents: Vec<(i32, i32)> = vec![];
+        if v == "*" {
+            let len = v.len();
+            gear_adjacents.push((k.0 as i32 - 1, k.1 as i32 - 1));
+            gear_adjacents.push((k.0 as i32, k.1 as i32 - 1));
+            gear_adjacents.push((k.0 as i32 + 1, k.1 as i32 - 1));
+
+            gear_adjacents.push((k.0 as i32 - 1, k.1 as i32 + len as i32));
+            gear_adjacents.push((k.0 as i32, k.1 as i32 + len as i32));
+            gear_adjacents.push((k.0 as i32 + 1, k.1 as i32 + len as i32));
+
+            for i in 0..len {
+                gear_adjacents.push((k.0 as i32 - 1, k.1 as i32 + i as i32));
+
+                gear_adjacents.push((k.0 as i32 + 1, k.1 as i32 + i as i32));
+            }
+            let mut items = BTreeSet::new();
+            for adj in gear_adjacents {
+                let (x, y) = (adj.0 as usize, adj.1 as usize);
+                if grid.contains_key(&(x, y)) {
+                    let adjascent = grid.get(&(x, y)).unwrap();
+                    if adjascent.parse::<usize>().is_ok() {
+                        items.insert(adjascent.parse::<usize>().unwrap());
+                    }
+                }
+            }
+            if items.len() == 2 {
+                results.push(items.iter().fold(1, |acc, x| acc * x));
+            }
+        }
+    }
+
+    results.iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT_TEXT: &str = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(process(INPUT_TEXT), 467835);
+    }
+}
